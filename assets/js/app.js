e8194f6e2adb33708ac0a0f779c2542606261bdd
Manuel Comi <mcomi@hotmail.com>
2020-04-19 17:51:34 -0500

    completed sections structure

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,17 @@
+$(".collapse").on("show.bs.collapse", function () {
+  console.log($(this));
+  $(this).addClass("active");
+  $(this)
+    .siblings(".card-header")
+    .find("img.collapse-icon")
+    .attr("src", "assets/img/icons/minus.png");
+});
 
+$(".collapse").on("hide.bs.collapse", function () {
+  console.log("$(this)");
+  $(this).removeClass("active");
+  $(this)
+    .siblings(".card-header")
+    .find("img.collapse-icon")
+    .attr("src", "assets/img/icons/plus.png");
+});
